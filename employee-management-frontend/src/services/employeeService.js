a895f7fba5023fce38def817949c7eafce1ff16d
Bhimala Divakar <bheemaladivakar@gmail.com>
2026-07-24 17:26:07 +0530

    Fix employee API endpoint

--- employee-management-frontend/src/services/employeeService.js
+++ employee-management-frontend/src/services/employeeService.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = "https://employeemanagementsystem-o3vo.onrender.com";
+const BASE_URL = "https://employeemanagementsystem-o3vo.onrender.com/api/employees";
 
 export const getAllEmployees = () => axios.get(BASE_URL);
 
-export const getEmployeeById = (id) => axios.get(`${BASE_URL}/${id}`);
+export const getEmployeeById = (id) =>
+    axios.get(`${BASE_URL}/${id}`);
 
-export const addEmployee = (employee) => axios.post(BASE_URL, employee);
+export const addEmployee = (employee) =>
+    axios.post(BASE_URL, employee);
 
 export const updateEmployee = (id, employee) =>
     axios.put(`${BASE_URL}/${id}`, employee);
